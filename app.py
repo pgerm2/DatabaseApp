afaa25f667fbcafd0cc6415f26c122c919f3b347
Phillip Germany II <59968773+pgerm2@users.noreply.github.com>
2024-06-22 20:13:59 +0000

    added html and css

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, session
 
 app = Flask(__name__) #create flack app
 #part of the url after the domain name
 @app.route('/') #registered a route to the app.
 def hello_world():
-    return 'Hello, World!'
+    return render_template ('home.html')
   
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True) #starts app
